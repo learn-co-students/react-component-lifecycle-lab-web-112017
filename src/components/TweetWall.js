1a86cf0fd1a7c5e188997a598b75f357ee598898
Flatiron School <leslie.hsu@flatironschool.com>
2018-02-03 15:36:09 -0500

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,18 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    console.log(this.props.newTweets)
+    this.setState({tweets: this.props.newTweets})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({tweets: [...nextProps.newTweets, ...this.state.tweets]})
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    nextProps.newTweets.length > 0
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
